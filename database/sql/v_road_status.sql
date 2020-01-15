c095a961e0ae47031acf87ea505affcd73f81889
analuthfa <luthfa.ana@gmail.com>
2020-01-15 14:17:28 +0700

    rename sql vroadstatus

--- database/sql/v_road_status.sql
+++
@@ -1,7 +0,0 @@
-create or replace view v_list_history_status
-select trs.id,trs.road_id ,vrl.road_code,vrl.road_name,vrl.total_length, vrl.status_name, vrl.category_name, trs.updated_by, trs.created_at, trs.updated_at
-from TR_ROAD_STATUS trs, V_ROAD_LOG vrl,
-TM_ROAD tmr 
-join TM_ROAD_STATUS tmrs on tmrs.id = (select status_id from TR_ROAD_STATUS trs where trs.road_id = tmr.id order by id desc limit 1)
-join TM_ROAD_CATEGORY tmrc on tmrc.id = (select category_id from TR_ROAD_STATUS trs where trs.road_id = tmr.id order by id desc limit 1)
-where vrl.id = trs.road_id and tmr.id = vrl.id and tmr.deleted_at is null 
