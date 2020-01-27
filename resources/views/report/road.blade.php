077c4caf9f77977d76cf040191c877d93c88979d
analuthfa <luthfa.ana@gmail.com>
2020-01-27 17:19:55 +0700

    exportoption

--- resources/views/report/road.blade.php
+++ resources/views/report/road.blade.php
@@ -212,6 +212,12 @@ function loadGrid(){
                             {   extend: 'excel', 
                                 text : '<i class="icon-file-excel"></i> Export to Excel',
                                 className: 'btn bg-teal-400 btn-labeled btexcel',
+                                exportOptions: {
+                                                modifier: {
+                                                page: 'all',
+                                                search: 'none'   
+                                                }
+                                },
                              }
                         ]
                     },
