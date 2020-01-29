15d841fd00e2aac7f55c850aa7d9bae775273ec0
andipahlevy <andilevi@gmail.com>
2020-01-29 14:38:03 +0700

    fix view segment

--- database/sql/v_list_progress_perkerasan_jalan.sql
+++ database/sql/v_list_progress_perkerasan_jalan.sql
@@ -5,6 +5,7 @@ concat(round(ifnull((select sum(length) from TR_ROAD_PAVEMENT_PROGRESS trpp wher
 (select total_length from TR_ROAD_LOG trl where trl.road_id = tmr.id order by id desc limit 1)*100,2),'%') as progress,
 (select total_length from TR_ROAD_LOG trl where trl.road_id = tmr.id order by id desc limit 1) as total_length,
 (select asset_code from TR_ROAD_LOG trl where trl.road_id = tmr.id order by id desc limit 1) as asset_code,
+(select segment from TR_ROAD_STATUS trs where trs.road_id = tmr.id order by id desc limit 1) as segment,
 tmr.* from TM_ROAD tmr 
 join TM_COMPANY tmc on tmc.company_code = tmr.company_code
 join TM_ESTATE tme on tme.werks = tmr.werks
