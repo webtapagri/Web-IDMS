9bb4685e02cf74a2d74c0481edb0db41f5bbec01
andipahlevy <andilevi@gmail.com>
2020-02-04 17:31:50 +0700

    tuning

--- database/sql/v_road_status.sql
+++ database/sql/v_road_status.sql
@@ -1,7 +1,10 @@
 create or replace view V_LIST_HISTORY_STATUS as
-select trs.id,trs.road_id ,vrl.road_code,vrl.road_name,vrl.total_length, vrl.status_name, vrl.category_name, trs.updated_by, trs.created_at, trs.updated_at
-from TR_ROAD_STATUS trs, V_ROAD_LOG vrl,
+select trs.status_id, trs.id,trs.road_id ,trs.road_code,trs.road_name,vrl.total_length, tmrs.status_name
+, tmrc.category_name, trs.updated_by
+, trs.created_at, trs.updated_at, trs.segment
+from TR_ROAD_STATUS trs
+join TM_ROAD_STATUS tmrs on tmrs.id = trs.status_id
+join TM_ROAD_CATEGORY tmrc on tmrc.id = trs.category_id
+, V_ROAD_LOG vrl,
 TM_ROAD tmr 
-join TM_ROAD_STATUS tmrs on tmrs.id = (select status_id from TR_ROAD_STATUS trs where trs.road_id = tmr.id order by id desc limit 1)
-join TM_ROAD_CATEGORY tmrc on tmrc.id = (select category_id from TR_ROAD_STATUS trs where trs.road_id = tmr.id order by id desc limit 1)
 where vrl.id = trs.road_id and tmr.id = vrl.id and tmr.deleted_at is null 
