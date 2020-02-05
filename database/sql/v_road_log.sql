6929bfd849987a97b500c34f6e06586ef94a565f
andipahlevy <andilevi@gmail.com>
2020-02-05 16:53:23 +0700

    remove join in view

--- database/sql/v_road_log.sql
+++ database/sql/v_road_log.sql
@@ -8,10 +8,6 @@ tmr.asset_code,
 tmr.segment,
 tmr.id, tmr.company_code, tmr.werks, tmr.afdeling_code, tmr.block_code, tmr.road_code, tmr.road_name, tmr.status_pekerasan, tmr.status_aktif, tmr.deleted_at, tmr.created_at, tmr.updated_at, tmr.estate_code, tmr.updated_by 
 from TM_ROAD tmr 
-join TM_COMPANY tmc on tmc.company_code = tmr.company_code
-join TM_ESTATE tme on tme.werks = tmr.werks
-join TM_AFDELING tma on tma.afdeling_code = tmr.afdeling_code and tma.werks = tmr.werks
-left join TM_BLOCK tmb on tmb.block_code = tmr.block_code and tmb.werks = tmr.werks
 join TM_ROAD_STATUS tmrs on tmrs.id = tmr.status_id
 join TM_ROAD_CATEGORY tmrc on tmrc.id = tmr.category_id
 where tmr.deleted_at is null 
