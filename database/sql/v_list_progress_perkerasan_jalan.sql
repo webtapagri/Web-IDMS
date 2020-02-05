394f07b805c0e8a46f4372db171c143a295311d6
analuthfa <luthfa.ana@gmail.com>
2020-02-05 16:28:47 +0700

    left join list report&transaction

--- database/sql/v_list_progress_perkerasan_jalan.sql
+++ database/sql/v_list_progress_perkerasan_jalan.sql
@@ -11,7 +11,7 @@ from TM_ROAD tmr
 join TM_COMPANY tmc on tmc.company_code = tmr.company_code
 join TM_ESTATE tme on tme.werks = tmr.werks
 join TM_AFDELING tma on tma.afdeling_code = tmr.afdeling_code and tma.werks = tmr.werks
-join TM_BLOCK tmb on tmb.block_code = tmr.block_code and tmb.werks = tmr.werks
+left join TM_BLOCK tmb on tmb.block_code = tmr.block_code and tmb.werks = tmr.werks
 join TM_ROAD_STATUS tmrs on tmrs.id = tmr.status_id
 join TM_ROAD_CATEGORY tmrc on tmrc.id = tmr.category_id
 where tmr.deleted_at is null
