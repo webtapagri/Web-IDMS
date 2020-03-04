18cce4b5a7cb560d97af5a724d713c748e8e4ec3
analuthfa <luthfa.ana@gmail.com>
2020-03-04 11:42:06 +0700

    table alignment

--- resources/views/report/summary_perkerasan.blade.php
+++ resources/views/report/summary_perkerasan.blade.php
@@ -27,68 +27,68 @@ table.table.table-condensed {
 	<div class="col-sm-7">
 		<table class="table table-condensed">
 			<tr>
-				<th colspan="3">Jalan Produksi</th>
+				<th colspan="4">Jalan Produksi</th>
 			</tr>
 			<tr>	
 				<td></td>
 				<td>Total Jalan Produksi</td>
-				<td>{{  number_format($data['jum_produksi']) }} Unit</td>
+				<td align="right">{{  number_format($data['jum_produksi']) }}</td> <td>Unit</td>
 			</tr>
 			<tr>	
 				<td></td>
 				<td>&Sigma; Panjang Jalan Produksi</td>
-				<td>{{  number_format($data['len_produksi']) }}  M</td>
+				<td align="right">{{  number_format($data['len_produksi']) }}</td> <td>  M</td>
 			</tr>
 			<tr>	
 				<td></td>
 				<td>&Sigma; Panjang Jalan Sudah Diperkeras</td>
-				<td>{{  number_format($data['len_pavement']) }} M</td>
+				<td align="right">{{  number_format($data['len_pavement']) }}</td> <td> M</td>
 			</tr>
 			<tr>	
 				<td></td>
 				<td>&Sigma; Panjang Jalan Belum Diperkeras</td>
-				<td>{{  number_format($data['len_produksi'] - $data['len_pavement']) }} M</td>
+				<td align="right">{{  number_format($data['len_produksi'] - $data['len_pavement']) }}</td> <td> M</td>
 			</tr>
 
 			<tr>
-				<th colspan="3">Jalan Non Produksi</th>
+				<th colspan="4">Jalan Non Produksi</th>
 			</tr>
 			<tr>	
 				<td></td>
 				<td>Total Jalan Non Produksi</td>
-				<td>{{  number_format($data['jum_non_produksi']) }} Unit</td>
+				<td align="right">{{  number_format($data['jum_non_produksi']) }}</td> <td> Unit</td>
 			</tr>
 			<tr>	
 				<td></td>
 				<td>&Sigma; Panjang Jalan Non Produksi</td>
-				<td>{{  number_format($data['len_non_produksi']) }} M</td>
+				<td align="right">{{  number_format($data['len_non_produksi']) }}</td> <td> M</td>
 			</tr>
 
 			<tr>
-				<th colspan="3">Jalan Umum</th>
+				<th colspan="4">Jalan Umum</th>
 			</tr>
 			<tr>	
 				<td></td>
 				<td>Total Jalan Umum</td>
-				<td>{{  number_format($data['jum_umum']) }} Unit</td>
+				<td align="right">{{  number_format($data['jum_umum']) }}</td> <td> Unit</td>
 			</tr>
 			<tr>	
 				<td></td>
 				<td>&Sigma; Panjang Jalan Umum</td>
-				<td>{{  number_format($data['len_umum']) }} M</td>
+				<td align="right">{{  number_format($data['len_umum']) }}</td> <td> M</td>
 			</tr>
 		</table>
 	</div>
 	<div class="col-sm-4">
 		<table class="table table-condensed">
 			<tr>
-				<th colspan="2">Data Perkerasan</th>
+				<th colspan="3">Data Perkerasan</th>
 			</tr>
 			@foreach($data['perkerasan'] as $dp)
    
 			<tr>	
 				<td>{{ $dp->bulan }}</td>
-				<td>{{ number_format($dp->len_perkerasan) }} M</td>
+				<td align="right">{{ number_format($dp->len_perkerasan) }}</td> <td> M</td>
 			</tr>
 			@endforeach
 	</table>
